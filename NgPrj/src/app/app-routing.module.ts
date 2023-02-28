7f47c94daaf3c343bfa971cca3f5dbbcd4c0b9fa
Dmitriy Kemenov <dmitriy.kemenov@icl-services.com>
2023-02-28 09:48:42 +0300

    Added header, products and shopping cart components

--- NgPrj/src/app/app-routing.module.ts
+++ NgPrj/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: 'products', component: ProductsComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
